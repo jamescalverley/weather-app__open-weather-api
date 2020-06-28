57951f919572761d8cecc7773b0e57cc19858b47
James Calverley <james.calverley3@gmail.com>
2020-06-28 18:40:24 -0400

    adds ajax GET requests for current and forecast weather data

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 
 
-getWeatherData("Toronto")
+currentWeather("Toronto");
+fiveDayForecast("Toronto");
 
-async function getWeatherData(searchCity){
-    console.log("[getWeatherData fn ]")
+function currentWeather(searchCity){
+    console.log("[currentWeather fn ]");
 
     let apiKey = "4e033b3f0bf4413196c595a89671e437";
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}`;
 
-    let response = await fetch(queryURL);
-    let weatherData = response.json();
-    
-    console.log("API DATA", weatherData)
+    $.ajax({
+        url: queryURL, 
+        method: "GET"
+    }).then( ( response ) => {
+        console.log("Current weather data >>", response )
+    } );
+
 };
 
-function currentWeather(){
-    console.log("[currentWeather fn ] ")
-};
+function fiveDayForecast(searchCity){
+    console.log("[fiveDayForcast fn ]");
 
-function fiveDayForecast(){
-    console.log("[fiveDayForcast fn ]")
+    let apiKey = "4e033b3f0bf4413196c595a89671e437";
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${apiKey}`;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then( ( response ) => {
+        console.log("5 day forecast data >>", response)
+    });
+
 };
