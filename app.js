b42197354af3af943a931c08b6b9fc8f2e15daf2
James Calverley <james.calverley3@gmail.com>
2020-06-27 22:30:14 -0400

    adds weather api call

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 
 
-function getWeatherData(){
+getWeatherData("Toronto")
+
+async function getWeatherData(searchCity){
     console.log("[getWeatherData fn ]")
+
+    let apiKey = "4e033b3f0bf4413196c595a89671e437";
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}`;
+
+    let response = await fetch(queryURL);
+    let weatherData = response.json();
+    
+    console.log("API DATA", weatherData)
 };
 
 function currentWeather(){
