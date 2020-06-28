4b21c63d359f7aa776e247a9d2f190e0c31e69f6
James Calverley <james.calverley3@gmail.com>
2020-06-27 22:09:58 -0400

    sets up file structure

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+function getWeatherData(){
+    console.log("[getWeatherData fn ]")
+};
+
+function currentWeather(){
+    console.log("[currentWeather fn ] ")
+};
+
+function fiveDayForecast(){
+    console.log("[fiveDayForcast fn ]")
+};
