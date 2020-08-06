31e28e343e88bf8f3f71507cd95a7a3fab07e1c7
James Calverley <james.calverley3@gmail.com>
2020-08-06 13:50:30 -0400

    cleans up code (app.js)

--- public/app.js
+++ public/app.js
@@ -3,14 +3,11 @@ let searchCities = [];
 let apiKey = "4e033b3f0bf4413196c595a89671e437";
 
 async function currentWeather(searchCity){
-   // console.log("[currentWeather fn ]");    
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}`;
     $.ajax({
         url: queryURL, 
         method: "GET"
     }).then( ( response ) => {
-        //console.log("Current weather data >>", response )
-    
         let currentData = {
             cityName: response.name, 
             description: response.weather[0].description,
@@ -23,20 +20,15 @@ async function currentWeather(searchCity){
             coordLAT: response.coord.lat, 
             coordLON: response.coord.lon    
         };
-        
         $("#cur-city-name-t").text(currentData.cityName);
         $("#cur-description-t").text(currentData.description);
         $("#cur-date-t").text(moment().format("dddd MMMM Do YYYY"));
         $("#current-icon").attr("src", selectIconCurrent(currentData.iconCode));
-
-        
         $("#cur-temp-t").html(`${currentData.temp}<span class="metric-unit-lg"> &degC</span>`);
         $("#cur-feelsLike-t").html(`${currentData.feelsLike}<span class="metric-unit-lg"> &degC</span>`);
         $("#cur-humidity-t").html(`${currentData.humidity}<span class="metric-unit-lg"> %</span>`);
         $("#cur-windspeed-t").html(`${currentData.windspeed}<span class="metric-unit-sm"> km/h</span>`);
         $("#cur-pressure-t").html(`${currentData.pressure}<span class="metric-unit-sm"> kpa</span>`);
-        
-        //console.log(currentData)
         saveCity( currentData.cityName );
         storeCities();
         getUVIndex(currentData.coordLAT, currentData.coordLON);
@@ -44,7 +36,6 @@ async function currentWeather(searchCity){
 };
 
 async function getUVIndex(coordLAT, coordLON){
-    //console.log(` [getUVIndex] : LAT: ${coordLAT} LON: ${coordLON}`)
     let queryURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${coordLAT}&lon=${coordLON}`
     $.ajax({
         url: queryURL,
@@ -56,14 +47,11 @@ async function getUVIndex(coordLAT, coordLON){
 };
 
 async function fiveDayForecast(searchCity){
-   // console.log("[fiveDayForcast fn ]");
     let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${apiKey}`;
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then( ( response ) => {
-        //console.log("5 day forecast data >>", response)
-    // create array of objects to hold all forecast data
     let forecastData = [
         {
             dateDay: moment(response.list[3].dt_txt).format("dddd"),
@@ -111,11 +99,9 @@ async function fiveDayForecast(searchCity){
             humidity: response.list[35].main.humidity        
         }
     ]; 
-    //console.log(forecastData)  
     let forecastDisplay = document.getElementById('forecast-weather-t');
     let degree = String.fromCharCode(176);
     forecastDisplay.innerHTML = "";
-
     forecastData.forEach( (forecastDay) => {
         forecastDisplay.innerHTML += `
         <div class="forecast-day-t">
@@ -132,8 +118,6 @@ async function fiveDayForecast(searchCity){
     });
 };
 
-// ASSIGNS WEATHER ICONS
-
 function selectIconCurrent(code) {
     const filePath = "./Assets/current/";
     const icon = {
@@ -148,36 +132,26 @@ function selectIconCurrent(code) {
         thunderstorm: "thunderstorm-128.png"
     };
     if( code >= 200 && code <= 232) {
-        //console.log("ICON == thunderstorm")
         return filePath + icon.thunderstorm
     } if( code >= 300 && code <= 321) {
-        //console.log("ICON == shower-rain")
         return filePath + icon.showerRain
     } if( code >= 500  && code <= 531) {
-        //console.log("ICON == rain")
         return filePath + icon.rain
     } if( code >= 600  && code <= 622) {
-        //console.log("ICON == snow")
         return filePath + icon.snow
     } if( code >= 701  && code <= 781) {
-        //console.log("ICON == mist")
         return filePath + icon.mist
     } if( code == 800 ) {
-        //console.log("ICON == clear-sky")
         return filePath + icon.clearSky
     } if( code == 801 ) {
-        //console.log("ICON == few-clouds")
         return filePath + icon.fewClouds
     } if( code == 802 ) {
-        //console.log("ICON == scattered-clouds")
         return filePath + icon.scatteredClouds
     } if( code == 803 ) {
-        //console.log("ICON == broken-clouds")
         return filePath + icon.brokenClouds
     } if( code == 804 ) {
-        //console.log("ICON == broken-clouds (overcast)")
         return filePath + icon.brokenClouds
-    } 
+    }; 
 };
 
 function selectIconForecast(code) {
@@ -193,40 +167,29 @@ function selectIconForecast(code) {
         thunderstorm: "thunderstorm-48.png"
     };
     if( code >= 200 && code <= 232) {
-       // console.log("ICON == thunderstorm")
         return filePath + icon.thunderstorm
     } if( code >= 300 && code <= 321) {
-        //console.log("ICON == shower-rain")
         return filePath + icon.rain
     } if( code >= 500  && code <= 531) {
-        //console.log("ICON == rain")
         return filePath + icon.rain
     } if( code >= 600  && code <= 622) {
-        //console.log("ICON == snow")
         return filePath + icon.snow
     } if( code >= 701  && code <= 781) {
-        //console.log("ICON == mist")
         return filePath + icon.mist
     } if( code == 800 ) {
-       // console.log("ICON == clear-sky")
         return filePath + icon.clearSky
     } if( code == 801 ) {
-        //console.log("ICON == few-clouds")
         return filePath + icon.fewClouds
     } if( code == 802 ) {
-        //console.log("ICON == scattered-clouds")
         return filePath + icon.scatteredClouds
     } if( code == 803 ) {
-        //console.log("ICON == broken-clouds")
         return filePath + icon.brokenClouds
     } if( code == 804 ) {
-        //console.log("ICON == broken-clouds (overcast)")
         return filePath + icon.brokenClouds
-    } 
+    }; 
 };
 
 function handleClick(ev){
-    // ev.preventDefault();
     let searchValue = $('#searchField').val();
     if( !searchValue ) {
         console.log("no search value!")
@@ -245,17 +208,15 @@ document.addEventListener('keydown', (event) => {
     if( event.keyCode === 13 ) {
         handleClick();
         $('#searchField').val(""); 
-    }
+    };
 });
 
 function handleRecentSearch(recentCity){
-    //console.log("[handleRecentSearch fn ]");
     currentWeather(recentCity);
     fiveDayForecast(recentCity);
 };
 
 function saveCity( city ){
-    //console.log("**** running saveCity() >> saving", city)
     if( searchCities.indexOf(city) !== -1 ){
         return 
     } else {
@@ -275,17 +236,14 @@ function storeCities(){
 };
 
 function renderSearchedList(){
-   // console.log("[renderSearchedList]")  
    document.getElementById('recent-search').innerHTML = " "; 
    searchCities.forEach( (searchCity) => {
         document.getElementById('recent-search').innerHTML += `
-        <button class="btn btn-outline-secondary recent-btn" onClick="handleRecentSearch('${searchCity}')" type="button">${searchCity}</button>
-        `
-    })
+        <button class="btn btn-outline-secondary recent-btn" onClick="handleRecentSearch('${searchCity}')" type="button">${searchCity}</button>`
+    });
 };
 
 function clearSearches(){
-    console.log("----clearing recent searches")
     searchCities = [];
     localStorage.clear();
     renderSearchedList();
@@ -304,8 +262,6 @@ function displayClearBtn(){
 
 function hideNav(){
     let maxWidth = document.body.clientWidth;
-    console.log("clientWidth =", maxWidth);
-
     if( maxWidth > 970 ){
         $(".nav-burger").css("display", "none");
         $(".collapse").addClass("active").removeClass("collapse");
@@ -318,26 +274,20 @@ function hideNav(){
     };
 };
 
-window.addEventListener('resize', () => {
-    hideNav();
-})
+window.addEventListener('resize', hideNav);
 
 function themeSelect(toggle){
-    let theme = toggle;
-    console.log("[ themeSelect() ]", theme)
-    switch (theme) {
+    switch (toggle) {
         case "dark" : 
-            console.log("DARK")
             $(".light").addClass("dark").removeClass("light");
             $(".bg-light").addClass("bg-dark").removeClass("bg-light");
             break;
         case "light" : 
-            console.log("LIGHT")
             $(".dark").addClass("light").removeClass("dark");
             $(".bg-dark").addClass("bg-light").removeClass("bg-dark");
             break;
     };
-    storeTheme(theme);
+    localStorage.setItem("theme", toggle);
 };
 
 $("#darkToggle").on('click', () => {
@@ -347,34 +297,25 @@ $("#lightToggle").on('click', () => {
     themeSelect("light") 
 });
 
-function storeTheme(theme){
-    console.log("Storing theme:", theme)
-    localStorage.setItem("theme", theme)
-};
 function initTheme(){
     let savedTheme = localStorage.getItem("theme");
-    console.log("Saved theme:", savedTheme);
     if(savedTheme) {
         themeSelect(savedTheme);
-    } else if(!savedTheme) {
+    } else {
         themeSelect("light");
-    }
+    };
 };
 
 function init(){
     let storedCities = JSON.parse(localStorage.getItem("searchedCities:"));
-    //document.getElementById('clear-search').style.display = "none";
-    // console.log("getting from local storage >>>", storedCities )
     if( storedCities != null ) {
         searchCities = storedCities
     };
-    // console.log("searchCities", searchCities)
     hideNav();
     renderSearchedList();
     displayClearBtn();
     initTheme();
 };
-
 
 // for working on UI
 function uiWork(){
